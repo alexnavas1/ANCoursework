7bc17f1eaf02a31a2db399b9bdacaab9630c4d10
alexnavas1 <93232@farnborough.ac.uk>
2020-10-08 15:54:19 +0100

    Redo of class client, fixed the server duplicates but still have a space in Coursework Database.db

--- src/main/java/controllers/Client.java
+++ src/main/java/controllers/Client.java
@@ -17,14 +17,14 @@ import java.io.IOException;
 public class Client {
 
     @GET
-    @Path("client/img/{path}")
+    @Path("img/{path}")
     @Produces({"image/jpeg,image/png"})
     public byte[] getImageFile(@PathParam("path") String path) {
         return getFile("client/img/" + path);
     }
 
     @GET
-    @Path("client/js/{path}")
+    @Path("js/{path}")
     @Produces({"text/javascript"})
     public byte[] getJavaScriptFile(@PathParam("path") String path) {
         return getFile("client/js/" + path);
@@ -38,7 +38,7 @@ public class Client {
     }
 
     @GET
-    @Path("client/css/{path}")
+    @Path("css/{path}")
     @Produces({"text/css"})
     public byte[] getCSSFile(@PathParam("path") String path) {
         return getFile("client/css/" + path);
